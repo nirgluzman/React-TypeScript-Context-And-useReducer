689a20fb5ab34cbe3d8e24977dd6ea9278b3c246
nirgluzman <nir.gluzman@gmail.com>
2023-12-03 00:09:45 +0100

    enable start/stop timers

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,25 +1,32 @@
 import {useEffect, useRef, useState} from 'react';
 
 import Container from './UI/Container.tsx';
-import {type Timer as TimerProps} from '../store/timer-context.tsx';
+import {useTimersContext, type Timer as TimerProps} from '../store/timer-context.tsx';
 
 export default function Timer({name, duration}: TimerProps) {
-	const interval = useRef<number | null>(null); // store reference to TimerID in the scope of each component instance.
+	const interval = useRef<number | null>(null); // store a reference to TimerID in the scope of each component instance.
 	const [remainingTime, setRemainingTime] = useState(duration * 1000); // duration is in seconds, while setInterval is in milliseconds.
+
+	const {isRunning} = useTimersContext();
 
 	if (remainingTime <= 0 && interval.current) {
 		clearInterval(interval.current);
 	}
 
 	useEffect(() => {
-		const timerID = window.setInterval(function () {
-			setRemainingTime((prevTime) => prevTime - 50);
-		}, 50);
+		let timerID: number;
 
-		interval.current = timerID;
+		if (isRunning) {
+			timerID = window.setInterval(function () {
+				setRemainingTime((prevTime) => prevTime - 50);
+			}, 50);
 
+			interval.current = timerID;
+		} else if (interval.current) {
+			clearInterval(interval.current);
+		}
 		return () => clearInterval(timerID); // clean-up function - called right before useEffect is running again or the component unmounts.
-	}, []);
+	}, [isRunning]);
 
 	const formatRemainingTime = (remainingTime / 1000).toFixed(2);
 
