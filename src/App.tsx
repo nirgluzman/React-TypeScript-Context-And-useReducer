2a7cae417024a85bb1cef373c3750ee089ce5af4
nirgluzman <nir.gluzman@gmail.com>
2023-12-02 21:39:01 +0100

    wrap child components with TimersContextProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
+import TimersContextProvider from './store/timer-context.tsx';
+
 import AddTimer from './components/AddTimer.tsx';
 import Header from './components/Header.tsx';
 import Timers from './components/Timers.tsx';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <main>
-        <AddTimer />
-        <Timers />
-      </main>
-    </>
-  );
+	return (
+		<TimersContextProvider>
+			<Header />
+			<main>
+				<AddTimer />
+				<Timers />
+			</main>
+		</TimersContextProvider>
+	);
 }
 
 export default App;
