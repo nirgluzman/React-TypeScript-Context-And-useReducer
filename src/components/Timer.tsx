e7f3a5249e2bda6985ac129168641a1c14491a24
nirgluzman <nir.gluzman@gmail.com>
2023-12-03 12:02:51 +0100

    remove timerID variable as it is redundant

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -4,7 +4,7 @@ import Container from './UI/Container.tsx';
 import {useTimersContext, type Timer as TimerProps} from '../store/timer-context.tsx';
 
 export default function Timer({name, duration}: TimerProps) {
-	const interval = useRef<number | null>(null); // store a reference to TimerID in the scope of each component instance.
+	const interval = useRef<number | undefined>(); // stores a reference to TimerID in the scope of each component instance.
 	const [remainingTime, setRemainingTime] = useState(duration * 1000); // duration is in seconds, while setInterval is in milliseconds.
 
 	const {isRunning} = useTimersContext();
@@ -14,10 +14,8 @@ export default function Timer({name, duration}: TimerProps) {
 	}
 
 	useEffect(() => {
-		let timerID: number;
-
 		if (isRunning) {
-			timerID = window.setInterval(function () {
+			interval.current = window.setInterval(function () {
 				setRemainingTime((prevTime) => {
 					if (prevTime <= 0) {
 						return prevTime;
@@ -25,12 +23,10 @@ export default function Timer({name, duration}: TimerProps) {
 					return prevTime - 50;
 				});
 			}, 50);
-
-			interval.current = timerID;
 		} else if (interval.current) {
 			clearInterval(interval.current);
 		}
-		return () => clearInterval(timerID); // clean-up function - called right before useEffect is running again or the component unmounts.
+		return () => clearInterval(interval.current); // clean-up function - called right before useEffect() is running again or when the component unmounts.
 	}, [isRunning]);
 
 	const formatRemainingTime = (remainingTime / 1000).toFixed(2);
